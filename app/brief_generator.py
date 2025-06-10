d623d45bf95080f428a8cd8e05e6b8f0fed0517a
MeepShake <susselelle@hotmail.com>
2025-06-10 15:49:46 +0200

    cleanup

--- app/brief_generator.py
+++
@@ -1,6 +0,0 @@
-def generate_brief(graph, author_name):
-    coauthors = list(graph.neighbors(author_name))
-    brief = f"{author_name} har {len(coauthors)} kända samarbetspartners:\n"
-    for c in coauthors:
-        brief += f" - {c}\n"
-    return brief
--- app/relation_analyzer.py
+++
@@ -1,8 +0,0 @@
-def analyze_soft_links(graph):
-    indirect_links = {}
-    for node in graph.nodes:
-        neighbors = list(graph.neighbors(node))
-        for neighbor in neighbors:
-            second_degree = set(graph.neighbors(neighbor)) - {node}
-            indirect_links[node] = indirect_links.get(node, set()) | second_degree
-    return indirect_links
